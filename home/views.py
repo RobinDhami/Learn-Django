a33e32df6f6df715953f9bd5eba0b792b9f297f3
RobinDhami <rozendhami69@gmail.com>
2024-06-05 20:45:04 +0545

    form

--- home/views.py
+++ home/views.py
@@ -1,25 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login , logout 
+
 # Create your views here.
 
 def home(request):
-    peoples=[{'name':'Rabin Dhami','age':'27'},
-             {'name':'karan yogi','age':'17'},
-             {'name':'yogesh gwaje','age':'24'},
-             {'name':'Rabin kjjk','age':'20'}]
-    return render (request,"index.html", context={'peoples':peoples})
+    peoples = [
+        {'name': 'Rabin Dhami', 'age': '27'},
+        {'name': 'karan yogi', 'age': '17'},
+        {'name': 'yogesh gwaje', 'age': '24'},
+        {'name': 'Rabin kjjk', 'age': '20'}
+    ]
+    return render(request, "index.html", context={'peoples': peoples})
 
 def about(request):
-    return render(request,'about.html')
+    return render(request, 'about.html')
 
 def profile(request):
-    return render(request,'profile.html')
+    return render(request, 'profile.html')
 
 def contact(request):
-    return render(request,'contact.html')
+    return render(request, 'contact.html')
 
-def login(request):
-    return render(request,'login.html')
+def login_view(request):
+    return render(request, 'login.html')
 
-def register(request):
-    return render(request,'register')
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return 'success'    
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
