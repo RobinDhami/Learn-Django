5885eaa17ff2f41ad0507752bb5217de5e9a9f06
RobinDhami <rozendhami69@gmail.com>
2024-06-08 23:43:52 +0545

    get data from backend

--- home/models.py
+++ home/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Student(models.Model):
     name=models.CharField(max_length=100)
     image= models.ImageField(upload_to="stud")
+    email=models.EmailField(unique=True,default="")
+    bio= models.TextField(blank=True)
+    phone= models.CharField(max_length=20, null=True, blank=True)  
