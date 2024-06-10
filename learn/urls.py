5ea0bd18f88ae10ec427ebc69eb6136c980b26bd
RobinDhami <rozendhami69@gmail.com>
2024-06-10 23:07:06 +0545

    media work done

--- learn/urls.py
+++ learn/urls.py
@@ -25,3 +25,14 @@ urlpatterns = [
     path('register/',signup,name='login'),
     path('admin/', admin.site.urls),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                    document_root = settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()    
+    
