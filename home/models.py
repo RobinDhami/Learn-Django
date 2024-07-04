0269375d5b3a0ea48b890ffd6e070dd0462f9c1e
RobinDhami <rozendhami69@gmail.com>
2024-07-04 21:45:04 +0545

    models

--- home/models.py
+++ home/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 #Learn concept of aggregate foreign key annotate learn how to play with data
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
 class Student(models.Model):
     User= models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True) #This is used to authenticate user
     name=models.CharField(max_length=100)
